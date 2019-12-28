5e165f6facc8fd09b54a8361a9f4ff0148bd937c
柯宜賢 <56106834+ro123ro456g@users.noreply.github.com>
2019-12-28 15:06:30 +0800

    建立專案_20191228 1458
    ASP.net core 2.2 MVC專案建立
    WebAPI 專案建立

---
+++ Northwind-KP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Northwind_KP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
