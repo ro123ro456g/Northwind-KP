2114b4b1557f18ada83bf6b950625cab2a004bd4
柯宜賢 <56106834+ro123ro456g@users.noreply.github.com>
2019-12-29 23:31:02 +0800

    Client_20191229 2330
    Create更新中...
    卡圖片轉2進位未完成

--- WebApi/Metadata/CategoriesMetadata.cs
+++ WebApi/Metadata/CategoriesMetadata.cs
@@ -1,10 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Web;
 
 namespace WebApi.Models
 {
     internal class CategoriesMetadata
     {
+        //public HttpPostedFileBase Picture { get; set; }
+
         [JsonIgnore]
         public virtual ICollection<Products> Products { get; set; }
     }
